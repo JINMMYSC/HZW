c4f6633c04cdc892cb2a9f7fa6419272efada49e
JINMMYSC <oneyouhome@live.com>
2026-09-15 03:25:52 +0800

    Extend Windows smoke test through map download

--- scripts/test_protocol.bat
+++ scripts/test_protocol.bat
@@ -3,7 +3,7 @@ setlocal
 cd /d "%~dp0.."
 python -m unittest discover -s tests -v
 if errorlevel 1 pause & exit /b 1
-start "HZW V860 server" cmd /k "cd /d \"%CD%\" && python server.py --debug"
+start "HZW V860 Phase3 server" cmd /k "cd /d \"%CD%\" && python phase3_server.py --debug"
 timeout /t 2 /nobreak >nul
-python tools\smoke_client.py
+python tools\smoke_world_client.py
 pause
